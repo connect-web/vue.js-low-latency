7a426d2575448e11628fe44c455c08eeff401425
Elliot <141949359+webElliot@users.noreply.github.com>
2024-11-07 14:55:23 +0000

    Updated Leaderboard service to use activity API's with caching to save data based on activity / skill / id

--- src/services/LeaderboardService.js
+++ src/services/LeaderboardService.js
@@ -1,53 +1,69 @@
 class LeaderboardService {
     constructor() {
-        this.path = "http://localhost:4050"
+        this.path = "http://localhost:4050/api/v2/public/"
         this.apiUrls = {
-            toplist: '/api/v2/public/skill-toplist',
-            skillUsers: '/api/v2/public/skill-toplist-users',
+            skillToplist: 'skill-toplist',
+            skillUsers: 'skill-toplist-users',
+            activityToplist: 'boss-minigame-toplist',
+            activityUsers: 'boss-minigame-toplist-users',
+
         };
+        this.dataTypes = {
+            Skills: 'Skills',
+            Activities: 'Activities'
+        }
         this.dataCache = {
-            toplistData: [],
+            toplistData: {},
             playersData: {},
         };
-        this.totalPages = 0;
     }
 
     // Fetch toplist data and cache it
-    async fetchToplistData() {
-        if (this.dataCache.toplistData.length === 0) {
+    async fetchLeaderboard(type) {
+        if (!this.dataCache.toplistData[type]) {
+            // Initialize as an empty array if the key does not exist
+            this.dataCache.toplistData[type] = [];
+        }
+
+        if (this.dataCache.toplistData[type].length === 0) {
             try {
-                const response = await fetch(this.path+this.apiUrls.toplist, {credentials: 'include'});
-                this.dataCache.toplistData = await response.json();
-                // this.totalPages = Math.ceil(jsonData.length / 10);
+                const api_url = this.path + (type === this.dataTypes.Skills ? this.apiUrls.skillToplist : this.apiUrls.activityToplist);
+                const response = await fetch(api_url, {credentials: 'include'});
+                this.dataCache.toplistData[type] = await response.json();
+
             } catch (error) {
                 console.error('Error fetching toplist data:', error);
             }
         }
-        return this.dataCache.toplistData;
+        return this.dataCache.toplistData[type];
     }
 
     // Fetch player data for a specific skill ID, using caching to avoid redundant API calls
-    async fetchPlayersData(skillId) {
-        if (!this.dataCache.playersData[skillId]) {
+    async fetchPlayers(id, type) {
+        if (!this.dataCache.playersData[type]) {
+            // Initialize as an empty array if the type key does not exist
+            this.dataCache.playersData[type] = {};
+        }
+
+        if (!this.dataCache.playersData[type][id]) {
+            // Initialize as an empty array if the id key does not exist
+            this.dataCache.playersData[type][id] = [];
+        }
+
+        console.log(`dataTypes.Skills=[${this.dataTypes.Skills}] and type=[${type}]`);
+
+        if (this.dataCache.playersData[type][id].length === 0) {
             try {
-                const response = await fetch(`${this.path+this.apiUrls.skillUsers}?skill-id=${skillId}`, {credentials: 'include'});
-                const jsonData = await response.json();
-                this.dataCache.playersData[skillId] = jsonData;
+                const param_key = (type === this.dataTypes.Skills ? 'skill-id' : 'minigame-id');
+                const api_url = this.path + (type === this.dataTypes.Skills ? this.apiUrls.skillUsers : this.apiUrls.activityUsers) + `?${param_key}=${id}`;
+
+                const response = await fetch(api_url, {credentials: 'include'});
+                this.dataCache.playersData[type][id] = await response.json();
             } catch (error) {
                 console.error('Error fetching players data:', error);
             }
         }
-        return this.dataCache.playersData[skillId];
-    }
-
-    // Method to get cached toplist data directly if needed
-    getToplistData() {
-        return this.dataCache.toplistData;
-    }
-
-    // Method to get cached players data for a specific skill ID
-    getPlayersData(skillId) {
-        return this.dataCache.playersData[skillId] || [];
+        return this.dataCache.playersData[type][id];
     }
 }
 
