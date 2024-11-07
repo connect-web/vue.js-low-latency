64d993788689f8e8792ca428c2cf1d8abf50ec1d
Elliot <141949359+webElliot@users.noreply.github.com>
2024-11-07 14:54:46 +0000

    Updated Image helper to correctly format minigame icons added new function for number filter for shorter display for Table

--- src/utils/imageHelper.js
+++ src/utils/imageHelper.js
@@ -1,10 +1,15 @@
 // Formats item names into lowercase with underscores (e.g., "Hitpoints Strength" -> "hitpoints_strength")
-export function formatFileName(item) {
-    return item.replace(/[^a-zA-Z\s]/g, '').replace(/\s+/g, '_').toLowerCase();
+export function filterFileName(input){
+    let output = input.replace(/[^a-zA-Z\s]/g, '');
+    output = output.replaceAll(' ', '_')
+    return output
 }
 
 // Generates the image URL path
 export function getImageUrl(type, item) {
-    const folder = type === 'Skills' ? 'skill_icons' : 'minigame_icons';
-    return `/img/${folder}/${formatFileName(item)}.png`;
+    // Choose the image path based on the type
+    const iconFolder = (type === 'Skills' ? 'skill_icons' :'minigame_icons');
+    item = item.toLowerCase();
+    let item_name = (type === 'Skills' ? item : filterFileName(item));
+    return `/img/${iconFolder}/${item_name}.png`;
 }
--- src/utils/numberFilters.js
+++ src/utils/numberFilters.js
@@ -29,3 +29,18 @@ export function parseLargeNumber(value) {
     }
     return parseFloat(value) || 0;
 }
+
+
+export function viewTruncatedLargeNumbers(value) {
+    if (typeof value !== 'number') return value;
+    if (value >= 1_000_000_000_000) {
+        return `${(value / 1_000_000_000_000).toFixed(0)}T`;
+    } else if (value >= 1_000_000_000) {
+        return `${(value / 1_000_000_000).toFixed(0)}B`;
+    } else if (value >= 1_000_000) {
+        return `${(value / 1_000_000).toFixed(0)}M`;
+    } else if (value >= 1000) {
+        return `${(value / 1_000).toFixed(0)}K`;
+    }
+    return value.toString();
+}
